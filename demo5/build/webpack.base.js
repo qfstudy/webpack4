d1783107f209d6367824467e7b843001790b6c5f
qfstudy <qifeistudy@gmail.com>
2019-04-25 11:03:22 +0800

    配置文件拆分

---
+++ demo5/build/webpack.base.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports={
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname,'dist')
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]', 
+            outputPath: 'images/', 
+            limit: 2048           
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'postcss-loader' 
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use:[
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: true 
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new cleanWebpackPlugin(),
+  ]
+}
