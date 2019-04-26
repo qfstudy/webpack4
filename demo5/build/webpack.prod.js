3b8678089ad429c30dd351fcfd797eb548f8e863
qfstudy <qifeistudy@gmail.com>
2019-04-26 22:47:01 +0800

    拆分代码

---
+++ demo5/build/webpack.prod.js
@@ -0,0 +1,9 @@
+const merge = require('webpack-merge')
+const baseConfig=require('./webpack.base')
+
+const prodConfig={
+  mode: 'production', 
+  devtool: 'cheap-module-source-map'
+}
+
+module.exports=merge(baseConfig,prodConfig)
---
+++ demo5/src/math.js
@@ -0,0 +1,9 @@
+export let square= (x) => {
+  console.log(x)
+  return x * x;
+}
+
+export let cube = (x) => {
+  console.log(x)
+  return x * x * x;
+}
